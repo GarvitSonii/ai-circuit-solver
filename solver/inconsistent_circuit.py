ebf2c59fecad9627fcd71bb4d73aa7f24d101151
GarvitSonii <120626286+GarvitSonii@users.noreply.github.com>
2025-09-27 15:04:31 +0530

    test with unsolvable or impossible circuits

---
+++ solver/inconsistent_circuit.py
@@ -0,0 +1,12 @@
+n=3
+
+r1=1
+r2=1
+v1=18
+v2=12
+
+graph={
+    0: [[1,1,r1], [2,0,v1], [2,0,v2]], #nodes connected to 0
+    1: [[0,1,r1], [2,1,r2]], #nodes connected to 1
+    2: [[0,0,v1],[0,0,v2], [1,1,r2]], #nodes connected to 2
+}
--- solver/pipeline.py
+++ solver/pipeline.py
@@ -3,7 +3,7 @@ from test import graph as test_graph, n as test_n
 from test_series_parallel import graph as test_graph_sp, n as test_n_sp
 from test_wheat_stone import graph as test_graph_ws, n as test_n_ws
 from multi_volt_source import graph as test_graph_mv, n as test_n_mv
-
+from inconsistent_circuit import graph as test_graph_ic, n as test_n_ic
 
 # ans=solver(test_n,test_graph)
 # print(ans)
@@ -14,5 +14,8 @@ from multi_volt_source import graph as test_graph_mv, n as test_n_mv
 # ans=solver(test_n_ws,test_graph_ws)
 # print(ans)
 
-ans=solver(test_n_mv,test_graph_mv)
+# ans=solver(test_n_mv,test_graph_mv)
+# print(ans)
+
+ans=solver(test_n_ic,test_graph_ic)
 print(ans)
